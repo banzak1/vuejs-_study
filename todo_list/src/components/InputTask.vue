564cf03bae0072a3bf1e8202148c67dfbf481da3
banzak1 <leosantanabr@gmail.com>
2022-05-16 22:07:35 -0300

    criando função para adicionar tarefa

--- todo_list/src/components/InputTask.vue
+++ todo_list/src/components/InputTask.vue
@@ -1,13 +1,27 @@
 <template>
-  <input type="text" class="new-todo" placeholder="Oque presa ser feito?" />
+  <input
+    type="text"
+    class="new-todo"
+    placeholder="Oque presa ser feito?"
+    @keyup.enter="addTask"
+  />
 </template>
 
 <script>
-import Task from "../models/Taks.js";
+import { Task } from "../models/Taks.js";
 
 export default {
   data() {
     return {};
+  },
+
+  methods: {
+    addTask(value) {
+      let task = new Task();
+      task.completed = false;
+      task.title = value;
+      console.log("Deu bom");
+    },
   },
 };
 </script>
